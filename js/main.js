f501b6a40be61199a25ddb90e20a0600b8233159
Shubham202 <51954852+Shubham202@users.noreply.github.com>
2020-06-02 20:35:39 +0530

    Add initial website files

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const responsive = {
+    0: {
+        items:1
+    },
+    320: {
+        items:1
+    },
+    560: {
+        items: 2
+    },
+    960:{
+        items: 3
+    }
+}
+
+
+$(document).ready(function()
+{
+
+    $nav = $('.nav');
+    $togglecollapse = $('.toggle-collapse');
+
+    /*click event on togglecollapse*/
+    $togglecollapse.click(function()
+    {
+        $nav.toggleClass('collapse');
+    })
+
+    //owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+   //click to scroll
+   $('.move-up span').click(function(){
+       $('html,body').animate({
+       scrollTop: 0
+       }, 1500);
+   })
+
+   //Animaton on scroll (aos) instance
+   AOS.init();
+
+});
